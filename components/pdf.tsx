8adec3f178e2cc23bc593d4cc8a3e08cefbe2ede
Michael Hensler <michael.a.hensler@gmail.com>
2022-05-21 22:37:40 -0700

    hack

--- components/pdf.tsx
+++ components/pdf.tsx
@@ -1,6 +1,5 @@
 import styles from "../styles/Home.module.css";
 import Image, { StaticImageData } from "next/image";
-import { useCallback, useState } from "react";
 
 type PdfProps = {
   src: string;
@@ -10,22 +9,21 @@ type PdfProps = {
 };
 
 const Pdf = (props: PdfProps) => {
-  const [isPdf, setIsPdf] = useState(true);
-  const useFallback = useCallback(() => setIsPdf(false), []);
-
   return (
     <figure
-      className={isPdf ? styles.figure : `${styles.figure} ${styles.fallback}`}
+      className={
+        props.fallback ? `${styles.figure} ${styles.fallback}` : styles.figure
+      }
     >
-      <object
-        data={`${props.src}#page=${props.page ?? 1}&toolbar=0&view=FitH`}
-        type="application/pdf"
-        height={isPdf ? "100%" : "auto"}
-      >
-        {props.fallback ? (
-          <Image alt="" src={props.fallback} onLoad={useFallback} />
-        ) : null}
-      </object>
+      {props.fallback ? (
+        <Image alt="" src={props.fallback} />
+      ) : (
+        <object
+          data={`${props.src}#page=${props.page ?? 1}&toolbar=0&view=FitH`}
+          type="application/pdf"
+          height="100%"
+        />
+      )}
       <figcaption className={styles.figcaption}>
         <a href={props.src}>{props.caption ?? props.src}</a>
       </figcaption>
